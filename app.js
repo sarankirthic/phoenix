59d0c0426a50e047c0389dc43f5f668532470357
saran <spskartist@gmail.com>
2025-01-30 16:59:51 +0530

    changed port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { Gadget } = require("./models");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 
