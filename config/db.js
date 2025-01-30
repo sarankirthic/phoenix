3a87a25ab75046df63f9431bacc1ec1a53485e8e
saran <spskartist@gmail.com>
2025-01-30 14:06:12 +0530

    made minor security changes

--- config/db.js
+++ config/db.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
 const sequelize = new Sequelize({
     dialect: "postgres",
-    host: "localhost",
-    port: "5432",
-    database: "postgres1",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    // dialectOptions: {
+    //     ssl: true,
+    // }
 });
 
 sequelize
