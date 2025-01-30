5243c4afdc77fcc4029a75034ac0f0320c577150
saran <spskartist@gmail.com>
2025-01-30 14:15:11 +0530

    enabled ssl for database access

--- config/db.js
+++ config/db.js
@@ -8,9 +8,9 @@ const sequelize = new Sequelize({
     database: process.env.DB_DATABASE,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    // dialectOptions: {
-    //     ssl: true,
-    // }
+    dialectOptions: {
+        ssl: true,
+    }
 });
 
 sequelize
